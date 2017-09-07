3a27bd1956b0ca25e3bd4a85b3d6d866dbac9b0a
Douglas Matoso <doug2k1@gmail.com>
2017-09-07 00:55:50 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import buildSong from './songBuilder'
+
+const song = buildSong()
+document.querySelector('#songTitle').innerHTML = song.title
+document.querySelector('#songLyrics').innerHTML = song.lyrics
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
