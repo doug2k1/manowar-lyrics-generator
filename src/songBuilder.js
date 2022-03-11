ad409b9df8e79dfae2d999957a1373bcd2e73670
Douglas Matoso <doug2k1@gmail.com>
2022-03-11 20:51:01 -0300

    Update dependencies

--- src/songBuilder.js
+++ src/songBuilder.js
@@ -1,77 +1,77 @@
-import terms from "./terms";
-import { random, shuffle, compact, capitalize } from "lodash-es";
+import terms from './terms'
+import { random, shuffle, compact, capitalize } from 'lodash-es'
 
-const buildVerse = short => {
-  let verse = [];
+const buildVerse = (short) => {
+  let verse = []
 
   if (random(3) === 0) {
-    verse.push(shuffle(terms.subjects)[0]);
+    verse.push(shuffle(terms.subjects)[0])
   }
 
-  verse.push(shuffle(terms.verbs)[0]);
-  verse.push(shuffle(terms.adverbs)[0]);
+  verse.push(shuffle(terms.verbs)[0])
+  verse.push(shuffle(terms.pronouns)[0])
 
   if (random(2) === 0) {
-    verse.push(shuffle(terms.adjectives)[0]);
+    verse.push(shuffle(terms.adjectives)[0])
   }
 
-  verse.push(shuffle(terms.nouns)[0]);
+  verse.push(shuffle(terms.nouns)[0])
 
   if (random(2) === 0) {
-    verse.push(shuffle(terms.connectors)[0]);
+    verse.push(shuffle(terms.connectors)[0])
 
     if (random(3) === 3) {
-      verse.push(shuffle(terms.adjectives)[0]);
+      verse.push(shuffle(terms.adjectives)[0])
     }
 
-    verse.push(shuffle(terms.nouns)[0]);
+    verse.push(shuffle(terms.nouns)[0])
   }
 
   if (random(4) === 0 && !short) {
-    verse.push(shuffle(terms.connectors)[0]);
+    verse.push(shuffle(terms.connectors)[0])
 
     if (random(3) === 3) {
-      verse.push(shuffle(terms.adjectives)[0]);
+      verse.push(shuffle(terms.adjectives)[0])
     }
 
-    verse.push(shuffle(terms.nouns)[0]);
+    verse.push(shuffle(terms.nouns)[0])
   }
 
-  verse = compact(verse);
-  verse = capitalize(verse.join(" "));
+  verse = compact(verse)
+  verse = capitalize(verse.join(' '))
 
-  return verse;
-};
+  return verse
+}
 
 const buildSong = () => {
-  const title = buildVerse(true);
-  const lyrics = [];
+  const title = buildVerse(true)
+  const lyrics = []
 
   for (let i = 0; i < 6; i++) {
-    lyrics.push(buildVerse());
+    lyrics.push(buildVerse())
   }
 
-  lyrics.push("");
-  lyrics.push(title);
-  lyrics.push(buildVerse());
-  lyrics.push(title);
-  lyrics.push(buildVerse());
-  lyrics.push("");
+  lyrics.push('')
+  lyrics.push(title)
+  lyrics.push(buildVerse())
+  lyrics.push(title)
+  lyrics.push(buildVerse())
+  lyrics.push('')
 
   for (let i = 0; i < 4; i++) {
-    lyrics.push(buildVerse());
+    lyrics.push(buildVerse())
   }
 
-  lyrics.push("");
+  lyrics.push('')
 
   for (let i = 0; i < 4; i++) {
-    lyrics.push(title);
+    lyrics.push(title)
   }
 
   return {
     title: title,
-    lyrics: lyrics.join("<br>")
-  };
-};
+    lyrics: lyrics.join('<br>'),
+  }
+}
 
-export default buildSong;
+export default buildSong
